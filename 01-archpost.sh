8ac093188336bcb3e92fa6dd0ccd07be49a73a8b
atescula <34890552+atescula@users.noreply.github.com>
2021-11-04 09:18:52 +0200

    Add files via upload

---
+++ 01-archpost.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+sudo pacman -Syyu
+
+sudo pacman -S --needed intel-ucode
+sudo pacman -S --needed git base-devel curl wget rsync
+sudo pacman -S --needed go
+
+###### yay
+
+#rm -vrf ~/yay
+#cd ~/
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#makepkg -si
+#rm -rf ~/yay
+
+######
+
+#=========================== REMOVE some stuff
+sudo pacman -R --noconfirm aisleriot
+sudo pacman -R --noconfirm lftp
+sudo pacman -R --noconfirm grsync
+sudo pacman -R --noconfirm quodlibet
+sudo pacman -R --noconfirm exfalso
+sudo pacman -R --noconfirm exfatprogs
+
+
+#========================== REPO's
+
+echo -e "\nInstalling some fucking stuff\n"
+
+PKGS=(
+'adobe-source-code-pro-fonts'
+'adobe-source-sans-fonts'
+'adobe-source-serif-fonts'
+'amdvlk'
+'android-file-transfer'
+'audiocd-kio'
+'bash-completion'
+'bluez-utils'
+'bluez'
+'clipgrab'
+'cmatrix'
+'code'
+'digikam'
+'discover'
+'dvgrab'
+'exfat-utils'
+'ffmpeg'
+'ffmpegthumbs'
+'fuse2'
+'fuse3'
+'fuseiso'
+'gamemode'
+'gimp' # Photo editing
+'git'
+'gparted' # partition management
+'grub-customizer'
+'gst-libav'
+'gst-libav'
+'gst-libav'
+'gst-plugins-bad'
+'gst-plugins-good'
+'gst-plugins-good'
+'gst-plugins-ugly'
+'handbrake'
+'haveged'
+'hunspell-en_us'
+'hunspell-ro'
+'hunspell'
+'hyphen'
+'intel-media-sdk'
+'jdk-openjdk' # Java 17
+'kaccounts-providers'
+'kate'
+'kcodecs'
+'kcoreaddons'
+'kdegraphics-thumbnailers'
+'kdenlive'
+'kdeplasma-addons'
+'kdialog'
+'kfind'
+'kinfocenter'
+'kio-gdrive'
+'kompare'
+'krusader'
+'languagetool'
+'lib32-mesa'
+'libdvdcss'
+'libdvdcss'
+'libmythes'
+'libreoffice-fresh'
+'libva-mesa-driver'
+'libva-utils'
+'linux-firmware'
+'linux-headers'
+'linux'
+'mediainfo-gui'
+'mediainfo-gui'
+'mediainfo'
+'mesa'
+'mythes-en'
+'mythes-ro'
+'nano'
+'nfs-utils'
+'nload'
+'noto-fonts'
+'opentimelineio'
+'opentimelineio'
+'packagekit-qt5'
+'pacman-contrib'
+'perl-image-exiftool'
+'phonon-qt5-gstreamer'
+'picard'
+'rclone'
+'recordmydesktop'
+'recordmydesktop'
+'sane-airscan'
+'signal-desktop'
+'simplescreenrecorder'
+'sonnet'
+'spectacle'
+'speedtest-cli'
+'telegram-desktop'
+'terminus-font'
+'thunderbird'
+'ttf-caladea'
+'ttf-carlito'
+'ttf-dejavu'
+'ttf-liberation'
+'ttf-linux-libertine-g'
+'ttf-ubuntu-font-family'
+'vdpauinfo'
+'vlc'
+'vulkan-radeon'
+'vulkan-tools'
+'wavemon'
+'xf86-video-amdgpu'
+'xorg-apps'
+'xorg-drivers'
+'xorg-server'
+'xorg-xinit'
+'xorg-xkill'
+'xorg'
+'zeroconf-ioslave'
+#'archlinux-appstream-data'
+#'kde-plasma-addons'
+#'vulkan'
+
+#'virtualbox'
+#'virtualbox-host-modules-arch'
+#'virtualbox-guest-iso'
+)
+for PKG in "${PKGS[@]}"; do
+    echo "Installing now: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+
+
+
+
+
---
+++ 03-services.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+#==================== SERVICES========================
+clear
+
+
+echo -e "\nEnabling essential services"
+echo -e "\nEnabling essential services"
+
+sudo systemctl enable sddm.service
+sudo systemctl enable cups.service
+#ntpd -qg
+#systemctl enable ntpd.service
+sudo systemctl disable dhcpcd.service
+sudo systemctl stop dhcpcd.service
+
+sudo systemctl enable NetworkManager.service
+sudo systemctl enable bluetooth
+sudo systemctl enable teamviewerd.service
+sudo systemctl start teamviewerd.service
+
+sudo systemctl enable cronie.service
+sudo systemctl start cronie.service
+
+sudo modprobe vboxdrv
+sudo usermod -aG vboxsf $USER
+
+
+
+sudo echo "options snd_hda_intel power_save=0" | sudo tee -a /etc/modprobe.d/audio_disable_powersave.conf
