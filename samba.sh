74518b2ff6c0b488c5a2913e6b050d4897cab08f
atescula <atescula@gmail.com>
2021-12-06 16:51:28 +0200

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -5,6 +5,7 @@ sudo groupadd -f sambashare
 sudo smbpasswd -a $USER
 sudo gpasswd sambashare -a $USER
 sudo cp -f smb.conf /etc/samba/
+sudo cp -f nsswitch.conf /etc
 sudo systemctl enable smb
 sudo systemctl enable nmb
 sudo systemctl start smb
