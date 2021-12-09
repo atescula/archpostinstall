40602224723c0fe721a973e22bcff55636b03ff2
atescula <atescula@gmail.com>
2021-12-09 21:55:20 +0200

    Update 01-archpost.sh

--- 01-archpost.sh
+++ 01-archpost.sh
@@ -15,9 +15,13 @@ sudo pacman -R --noconfirm grsync
 sudo pacman -R --noconfirm quodlibet
 sudo pacman -R --noconfirm exfalso
 sudo pacman -R --noconfirm exfatprogs
-sudo pacman -R --noconfirm pulseaudio
+sudo pacman -R --noconfirm pulseaudio-equalizer
+sudo pacman -R --noconfirm pavucontrol-qt
+sudo pacman -R --noconfirm plasma-pa
+sudo pacman -R --noconfirm pulseaudio-bluetooth
 sudo pacman -R --noconfirm pulseaudio-alsa
 sudo pacman -R --noconfirm pulseaudio-jack
+sudo pacman -R --noconfirm pulseaudio
 
 #============================ install pipewire
 
